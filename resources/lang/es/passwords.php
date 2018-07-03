dc3c9021467571d4db046897399cd5bfb016c7d4
Ricardo Otxoa <ricardo@burman.es>
2018-07-03 14:25:09 +0200

    Add ES language folder and files

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe contener al menos seis caracteres y coincidir con su confirmación.',
+    'reset' => 'Su contraseña ha sido reiniciada',
+    'sent' => 'Acabamos de enviarle un enlace para reiniciar su contraseña. Revise su bandeja de entrada.',
+    'token' => 'El token de reinicio de contraseña no es correcto.',
+    'user' => "No existe ningún usuario con la dirección de correo introducida.",
+
+];
