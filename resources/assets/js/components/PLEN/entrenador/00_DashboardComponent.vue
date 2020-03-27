8dd0f2f3aed29acdd507464e602d07b45dd1d681
Ricardo Otxoa <ricardo@burman.es>
2020-03-27 13:15:52 +0100

    PLEN App: layout básico para Gestor y entrenador (menú lateral, menú responsive y área principal)

--- resources/assets/js/components/PLEN/entrenador/00_DashboardComponent.vue
+++ resources/assets/js/components/PLEN/entrenador/00_DashboardComponent.vue
@@ -1,22 +1,69 @@
 <template>
-  <div>
-    <h1>KAIXO MUNDUA!! Entrenador</h1>
-  </div>
+  <b-container fluid>
+
+    <b-row>
+
+      <b-col v-if="showMenu" xl="2" class="px-0">
+        <plen-navbar></plen-navbar>
+      </b-col>
+
+      <b-col :xl="showMenu ? 10 : 12" class="position-relative">
+        <a id="toggleButton" class="position-absolute" :title="showMenu ? 'Ocultar menú' : 'Mostrar menú'" @click="toggleMenu()">
+          <b-icon-double-left v-if="showMenu"></b-icon-double-left>
+          <b-icon-double-right v-else></b-icon-double-right>
+        </a>
+        <div class="p-xl-3">
+          <h1>KAIXO MUNDUA!! Entrenador</h1>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {
-  props: ['userRole'],
-  data () {
-    return {
+  import { BIconChevronDoubleLeft, BIconChevronDoubleRight } from 'bootstrap-vue';
+  import PlenNavbar from './components/navbar.vue';
+  import $ from 'jquery';
 
-    }
-  },
-  created() {
-    console.log("[CREATED] 2. this.userRole: " + this.$route.params.userRole);
+  export default {
+    data() {
+      return {
+        showMenu: true,
+      }
+    },
+    methods: {
+      toggleMenu() {
+        this.showMenu = !this.showMenu;
+      }
+    },
+    components: {
+      'b-icon-double-left': BIconChevronDoubleLeft,
+      'b-icon-double-right': BIconChevronDoubleRight,
+      'plen-navbar': PlenNavbar,
+    },
   }
-}
 </script>
 
 <style focused>
+  #toggleButton {
+    background-color:#ffffff;
+    border-bottom:2px solid #efefef;
+    border-right:2px solid #efefef;
+    cursor:pointer;
+    height:40px;
+    left:-0.1rem;
+    padding-top:.5rem;
+    text-align:center;
+    top:-1.5rem;
+    width:40px;
+
+    -webkit-transition:all .15s ease-in-out;
+    -moz-transition:all .15s ease-in-out;
+    -ms-transition:all .15s ease-in-out;
+    -o-transition:all .15s ease-in-out;
+    transition:all .15s ease-in-out;
+  }
+  #toggleButton:hover {
+    background-color:#fafafa;
+  }
 </style>
