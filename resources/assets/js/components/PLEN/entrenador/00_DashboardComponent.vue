bfc724d7bb3a030015e9b1516e4edde22924fa74
Ricardo Otxoa <ricardo@burman.es>
2020-04-03 11:20:34 +0200

    PLEN App - Gestor + Entrenador: mejoras enrutamiento y estilos de toggle button

--- resources/assets/js/components/PLEN/entrenador/00_DashboardComponent.vue
+++ resources/assets/js/components/PLEN/entrenador/00_DashboardComponent.vue
@@ -8,12 +8,12 @@
       </b-col>
 
       <b-col :xl="showMenu ? 10 : 12" class="position-relative">
-        <a id="toggleButton" class="position-absolute" :title="showMenu ? 'Ocultar menú' : 'Mostrar menú'" @click="toggleMenu()">
+        <a id="toggleButton" class="d-none d-xl-inline position-absolute" :title="showMenu ? 'Ocultar menú' : 'Mostrar menú'" @click="toggleMenu()">
           <i v-if="showMenu" class="fas fa-angle-double-left"></i>
           <i v-else class="fas fa-angle-double-right"></i>
         </a>
-        <div class="p-xl-3">
-          <h1>KAIXO MUNDUA!! Entrenador</h1>
+        <div id="render-route">
+          <router-view></router-view>
         </div>
       </b-col>
     </b-row>
@@ -30,6 +30,9 @@
         showMenu: true,
       }
     },
+    created() {
+      // this.$router.push({ name: 'PLEN_ENTRENADOR_Inicio' });
+    },
     methods: {
       toggleMenu() {
         this.showMenu = !this.showMenu;
@@ -42,17 +45,27 @@
 </script>
 
 <style focused>
+  #render-route {
+    left:0;
+    margin-top:-1.5rem;
+    position:absolute;
+    top:0;
+    width:100%;
+    z-index:0;
+  }
   #toggleButton {
-    background-color:#ffffff;
+    background-color:#444444;
     border-bottom:2px solid #efefef;
     border-right:2px solid #efefef;
+    color:#ffffff;
     cursor:pointer;
-    height:40px;
+    height:41px;
     left:-0.1rem;
     padding-top:.5rem;
     text-align:center;
     top:-1.5rem;
-    width:40px;
+    width:41px;
+    z-index:99;
 
     -webkit-transition:all .15s ease-in-out;
     -moz-transition:all .15s ease-in-out;
@@ -61,6 +74,15 @@
     transition:all .15s ease-in-out;
   }
   #toggleButton:hover {
-    background-color:#fafafa;
+    background-color:#747474;
+  }
+</style>
+
+<style>
+  .module-title {
+    background-color:#94c11f;
+    color:white;
+    font-weight: bold;
+    text-transform: uppercase;
   }
 </style>
--- resources/assets/js/components/PLEN/gestor/00_DashboardComponent.vue
+++ resources/assets/js/components/PLEN/gestor/00_DashboardComponent.vue
@@ -8,11 +8,11 @@
       </b-col>
 
       <b-col :xl="showMenu ? 10 : 12" class="position-relative">
-        <a id="toggleButton" class="position-absolute" :title="showMenu ? 'Ocultar menú' : 'Mostrar menú'" @click="toggleMenu()">
-          <b-icon-double-left v-if="showMenu"></b-icon-double-left>
-          <b-icon-double-right v-else></b-icon-double-right>
+        <a id="toggleButton" class="d-none d-xl-inline position-absolute" :title="showMenu ? 'Ocultar menú' : 'Mostrar menú'" @click="toggleMenu()">
+          <i v-if="showMenu" class="fas fa-angle-double-left"></i>
+          <i v-else class="fas fa-angle-double-right"></i>
         </a>
-        <div class="p-xl-3">
+        <div id="render-route">
           <router-view></router-view>
         </div>
       </b-col>
@@ -21,7 +21,6 @@
 </template>
 
 <script>
-  import { BIconChevronDoubleLeft, BIconChevronDoubleRight } from 'bootstrap-vue';
   import PlenNavbar from './components/navbar.vue';
   import $ from 'jquery';
 
@@ -40,25 +39,33 @@
       }
     },
     components: {
-      'b-icon-double-left': BIconChevronDoubleLeft,
-      'b-icon-double-right': BIconChevronDoubleRight,
       'plen-navbar': PlenNavbar,
     },
   }
 </script>
 
 <style focused>
+  #render-route {
+    left:0;
+    margin-top:-1.5rem;
+    position:absolute;
+    top:0;
+    width:100%;
+    z-index:0;
+  }
   #toggleButton {
-    background-color:#ffffff;
+    background-color:#444444;
     border-bottom:2px solid #efefef;
     border-right:2px solid #efefef;
+    color:#ffffff;
     cursor:pointer;
-    height:40px;
+    height:41px;
     left:-0.1rem;
     padding-top:.5rem;
     text-align:center;
     top:-1.5rem;
-    width:40px;
+    width:41px;
+    z-index:99;
 
     -webkit-transition:all .15s ease-in-out;
     -moz-transition:all .15s ease-in-out;
@@ -67,6 +74,15 @@
     transition:all .15s ease-in-out;
   }
   #toggleButton:hover {
-    background-color:#fafafa;
+    background-color:#747474;
+  }
+</style>
+
+<style>
+  .module-title {
+    background-color:#94c11f;
+    color:white;
+    font-weight: bold;
+    text-transform: uppercase;
   }
 </style>
