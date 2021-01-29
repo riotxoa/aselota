bd6131b3a754612a35751425a74ff7c1cef5c051
Ricardo Otxoa <ricardo@burman.es>
2021-01-29 10:10:50 +0100

    PLEN App - Gestor: mto.tabla auxiliar Tipos de Microciclo

--- app/Http/Controllers/PLEN_TipoMicrocicloController.php
+++ app/Http/Controllers/PLEN_TipoMicrocicloController.php
@@ -16,7 +16,7 @@ class PLEN_TipoMicrocicloController extends Controller
     {
       $request->user()->authorizeRoles(['admin', 'plen_gestor']);
 
-      $clientes = PLEN_TipoMicrociclo::orderBy('desc', 'asc')->get();
+      $clientes = PLEN_TipoMicrociclo::orderBy('order', 'asc')->get();
 
       return response()->json($clientes, 200);
     }
@@ -100,7 +100,7 @@ class PLEN_TipoMicrocicloController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, $id)
     {
         $request->user()->authorizeRoles(['admin', 'plen_gestor']);
 
