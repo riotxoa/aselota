c171ecebc455bef3cd62d3a7335c58380c6c1f8e
Ricardo Otxoa <ricardo@burman.es>
2021-03-18 10:50:09 +0100

    INFORMES: informe PDF de Pelotazos y Duración de partidos

--- app/Http/Controllers/InformesController.php
+++ app/Http/Controllers/InformesController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Festivale;
+use App\Festivale as Festival;
+use App\FestivalPartido as Partido;
+use App\FestivalPartidoPelotari as Pelotari;
+use App\Pelotari as PelotariBaiko;
+use App\PelotarisAspe as PelotariAspe;
+
+use Illuminate\Support\Facades\Log;
+
 use PDF; // https://www.positronx.io/laravel-pdf-tutorial-generate-pdf-with-dompdf-in-laravel/
 
 class InformesController extends Controller
@@ -11,15 +18,76 @@ class InformesController extends Controller
 
     public function GetPilotakadakEtaIraupena()
     {
-      // $pdf = \App::make('dompdf.wrapper');
-      // $pdf->loadHTML('<h1>Test</h1>');
-      // return $pdf->download('invoice.pdf');
+      $festivales = Festival::select(
+                      'festivales.id',
+                      'festivales.fecha',
+                      'festivales.hora',
+                      'frontones.name as fronton_name',
+                      'municipios.name as fronton_localidad'
+                    )
+                    ->leftJoin('frontones', 'festivales.fronton_id', 'frontones.id')
+                    ->leftJoin('municipios', 'frontones.municipio_id', 'municipios.id')
+                    ->get();
+
+      foreach( $festivales as $key => $festival ) {
+        $festival->partidos = $this->getPartidosByFestivalId($festival->id);
+      }
 
       $data = array(
-        'title' => 'IZENBURUA',
-        'desc' => 'AZALPENA',
+        'festivales' => $festivales
       );
+      return view('informes.pilotakadak-eta-iraupena', $data);
+      // $pdf = PDF::setPaper('a4', 'landscape')->loadView('informes.pilotakadak-eta-iraupena', $data);
       $pdf = PDF::loadView('informes.pilotakadak-eta-iraupena', $data);
       return $pdf->download('invoice.pdf');
+    }
+
+    private function getPartidosByFestivalId($id)
+    {
+      $partidos = Partido::select(
+                    'festival_partidos.id',
+                    'festival_partidos.orden',
+                    'campeonatos.name as campeonato',
+                    'tipo_partidos.name as tipo_partido',
+                    'festival_partidos.duracion',
+                    'festival_partidos.pelotazos',
+                    'festival_partidos.puntos_rojo',
+                    'festival_partidos.puntos_azul'
+                  )
+                  ->where('festival_id', $id)
+                  ->leftJoin('campeonatos', 'campeonatos.id', 'festival_partidos.campeonato_id')
+                  ->leftJoin('tipo_partidos', 'tipo_partidos.id', 'festival_partidos.tipo_partido_id')
+                  ->get();
+
+      foreach( $partidos as $key => $partido ) {
+        $partido->pelotaris_rojo = $this->getPelotarisByPartidoId($partido->id, 'R');
+        $partido->pelotaris_azul = $this->getPelotarisByPartidoId($partido->id, 'A');
+      }
+
+      return $partidos;
+    }
+
+    private function getPelotarisByPartidoId($id, $color)
+    {
+      $pelotaris = Pelotari::select(
+                'festival_partido_pelotaris.pelotari_id',
+                'festival_partido_pelotaris.asegarce',
+                'festival_partido_pelotaris.is_sustituto'
+               )
+               ->where('festival_partido_id', $id)
+               ->where('color', $color)
+               ->where('asiste', 1)
+               ->get();
+
+      foreach( $pelotaris as $pelotari ) {
+        if( $pelotari->asegarce ) {
+          $item = PelotariBaiko::where('id', $pelotari->pelotari_id)->first();
+        } else {
+          $item = PelotariAspe::where('id', $pelotari->pelotari_id)->first();
+        }
+        $pelotari->alias = ( $item ? $item->alias : 'N/A' );
+      }
+
+      return $pelotaris;
     }
 }
--- resources/views/informes/pilotakadak-eta-iraupena.blade.php
+++ resources/views/informes/pilotakadak-eta-iraupena.blade.php
@@ -2,11 +2,126 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>PILOTAKADAK ETA IRAUPENA</title>
+    <title>PELOTAZOS Y DURACIÓN</title>
+    <style>
+      h2,
+      table {
+        font-family:arial;
+      }
+      h2 {
+        line-height:1;
+        text-align:center;
+      }
+      h2 small {
+        font-size:65%;
+        font-weight:normal;
+      }
+      table {
+        font-size:14px;
+        margin-bottom:2rem;
+        width:100%;
+      }
+      th {
+        font-weight:bold;
+      }
+      table,
+      th,
+      td {
+        padding: 7.5px 10px;
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+      td.blue {
+        color:blue;
+      }
+      td.fecha,
+      td.hora {
+        text-align:center;
+      }
+      td.festival {
+        background-color:gray;
+        color:white;
+        width:25%;
+      }
+      td.partido {
+        width: 50%;
+      }
+      td.partido strong {
+        display:inline-block;
+        width:115px;
+      }
+      td.duracion,
+      td.pelotazos {
+        text-align:right;
+        width:12.5%;
+      }
+      td.puntos {
+        text-align:center;
+      }
+      td.red {
+        color:red;
+      }
+    </style>
   </head>
   <body>
-    <h1>PILOTAKADAK ETA IRAUPENA</h1>
-    <p>Title: {{ $title }}</p>
-    <p>Desc: {{ $desc }}</p>
+    <h2>PELOTAZOS Y DURACIÓN<br><small><?php echo date('d-m-Y'); ?></small></h2>
+      @foreach( $festivales as $festival )
+        <table style="border:none;padding:0;">
+          <tr>
+            <td colspan="4" class="festival">
+              <div style="display:inline-block; text-align:left; width:49.5%;">
+                <strong>[{{ $loop->iteration }}] Fecha y hora: </strong><?php $fecha = new DateTime($festival->fecha); echo $fecha->format('d-m-Y'); ?> - {{ $festival->hora }}
+              </div>
+              <div style="display:inline-block; text-align:right; width:49.5%;">
+                <strong>Frontón: </strong>{{ $festival->fronton_name }}&nbsp;({{ $festival->fronton_localidad }})
+              </div>
+            </td>
+          </tr>
+          @foreach( $festival->partidos as $partido )
+            <tr>
+              <td colspan="2" class="partido">
+                @if( $partido->campeonato )
+                  <strong>Campeonato: </strong>{{ $partido->campeonato }}<br/>
+                @endif
+                @if( $partido->tipo_partido )
+                  <strong>Tipo de partido: </strong>{{ $partido->tipo_partido }}
+                @endif
+              </td>
+              <td class="pelotazos">
+                <strong>Pelotazos</strong>
+              </td>
+              <td class="duracion">
+                <strong>Duración</strong>
+              </td>
+            </tr>
+            <tr>
+              <td class="red puntos">{{ $partido->puntos_rojo }}</td>
+              <td class="red">
+                @foreach( $partido->pelotaris_rojo as $pelotari )
+                  @if( $loop->iteration > 1 )
+                   &nbsp;-&nbsp;{{ $pelotari->alias }}<?php echo ( $pelotari->is_sustituto ? '<sup><small>*SUST.</small></sup>' : '' ); ?>
+                  @else
+                   {{ $pelotari->alias }}<?php echo ( $pelotari->is_sustituto ? '<sup><small>*SUST.</small></sup>' : '' ); ?>
+                  @endif
+                @endforeach
+              </td>
+              <td rowspan="2" class="pelotazos"><?php echo ( $partido->pelotazos ? $partido->pelotazos . ' pel.' : 'N/D' ); ?></td>
+              <td rowspan="2" class="duracion"><?php echo ( $partido->duracion ? $partido->duracion . ' min.' : 'N/D' ); ?></td>
+            </tr>
+            <tr>
+              <td class="blue puntos">{{ $partido->puntos_azul }}</td>
+              <td class="blue">
+                @foreach( $partido->pelotaris_azul as $pelotari )
+                  @if( $loop->iteration > 1 )
+                   &nbsp;-&nbsp;{{ $pelotari->alias }}<?php echo ( $pelotari->is_sustituto ? '<sup><small>*SUST.</small></sup>' : '' ); ?>
+                  @else
+                   {{ $pelotari->alias }}<?php echo ( $pelotari->is_sustituto ? '<sup><small>*SUST.</small></sup>' : '' ); ?>
+                  @endif
+                @endforeach
+              </td>
+            </tr>
+          @endforeach
+        </table>
+      @endforeach
   </body>
 </html>
