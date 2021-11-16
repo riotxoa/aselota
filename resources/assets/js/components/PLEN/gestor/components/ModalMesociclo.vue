e8a68c587df3372719b576a870946cffe29d836a
Ricardo Otxoa <ricardo@burman.es>
2021-11-16 11:19:38 +0100

    PLEN APP [Modal Mesociclos]: modularización de código JS reutilizable

--- resources/assets/js/components/PLEN/gestor/components/ModalMesociclo.vue
+++ resources/assets/js/components/PLEN/gestor/components/ModalMesociclo.vue
@@ -16,19 +16,16 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex';
   import FormMesociclo from './FormMesociclo';
+  import { mesociclo } from '../../common/functions.js';
 
   export default {
+    mixins: [ mesociclo ],
     data() {
       return {
-        mesociclo_backup: null,
         disabled: false
       }
     },
-    computed: mapState({
-      mesociclo: state => state.plen.mesociclo
-    }),
     created() {
       this.resetMesocicloBackup();
       this.$root.$on('disable-modal-mesociclo-save-button', this.toggleSaveButton);
@@ -38,41 +35,8 @@
         this.restoreMesocicloBackup();
         this.$root.$emit('cancelEditMesociclo');
       },
-      makeMesocicloBackup() {
-        this.mesociclo_backup.id = this.mesociclo.id;
-        this.mesociclo_backup.macrociclo_id = this.mesociclo.macrociclo_id;
-        this.mesociclo_backup.tipo_mesociclo_id = this.mesociclo.tipo_mesociclo_id;
-        this.mesociclo_backup.fecha_ini = this.mesociclo.fecha_ini;
-        this.mesociclo_backup.fecha_fin = this.mesociclo.fecha_fin;
-        this.mesociclo_backup.description = this.mesociclo.description;
-        this.mesociclo_backup.objetivos = this.mesociclo.objetivos;
-        this.mesociclo_backup.microciclos = this.mesociclo.microciclos;
-      },
       onShowModal() {
         this.makeMesocicloBackup();
-      },
-      resetMesocicloBackup() {
-        this.mesociclo_backup = {
-          id: null,
-          macrociclo_id: null,
-          tipo_mesociclo_id: null,
-          fecha_ini: null,
-          fecha_fin: null,
-          description: '',
-          objetivos: '',
-          microciclos: []
-        };
-      },
-      restoreMesocicloBackup() {
-        this.mesociclo.id = this.mesociclo_backup.id;
-        this.mesociclo.macrociclo_id = this.mesociclo_backup.macrociclo_id;
-        this.mesociclo.tipo_mesociclo_id = this.mesociclo_backup.tipo_mesociclo_id;
-        this.mesociclo.fecha_ini = this.mesociclo_backup.fecha_ini;
-        this.mesociclo.fecha_fin = this.mesociclo_backup.fecha_fin;
-        this.mesociclo.description = this.mesociclo_backup.description;
-        this.mesociclo.objetivos = this.mesociclo_backup.objetivos;
-        this.mesociclo.microciclos = this.mesociclo_backup.microciclos;
-        this.resetMesocicloBackup();
       },
       saveMesociclo() {
         this.$root.$emit('saveEditMesociclo');
