d03e5095be0ef3c6a7552c7069529b436c6e283a
Ricardo Otxoa <ricardo@burman.es>
2021-11-17 12:25:49 +0100

    PLEN APP [Dashboard]: reordenación de elementos en el layout

--- resources/assets/js/components/PLEN/gestor/10_InicioComponent.vue
+++ resources/assets/js/components/PLEN/gestor/10_InicioComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <b-row class="p-5">
-    <b-col cols="12" md="6" lg="5">
-      <CurrentItemsList />
+    <b-col cols="12" md="6">
+      <Calendar class="mr-md-1" />
     </b-col>
-    <b-col cols="12" md="6" lg="7">
-      <TodayTasks />
-      <Calendar />
+    <b-col cols="12" md="6">
+      <TodayTasks class="mt-3 mt-md-0" />
+      <CurrentItemsList class="mt-3"/>
     </b-col>
   </b-row>
 </template>
